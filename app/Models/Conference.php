e3a56736311cd7089d2d59920a5adac3f98c088d
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2021-02-21 21:18:24 +0700

    add relationship

--- app/Models/Conference.php
+++ app/Models/Conference.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Conference extends Model
 {
     use HasFactory;
+
+    protected $table = 'conferences';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'instansi',
+        'phone',
+        'twibbon_path',
+        'instagram_path',
+        'story_path',
+        'payment_proof',
+        'approver_id',
+        'approved_at',
+    ];
+
+    public function approver()
+    {
+        return $this->belongsTo(InternalProfile::class, 'approver_id', 'id');
+    }
 }
